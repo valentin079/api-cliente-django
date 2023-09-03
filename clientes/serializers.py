c6a196474d7347b37753eb5a3eb108741bb738a0
valentin079 <emerson.oliveira02@academico.ifs.edu.br>
2023-09-02 21:34:44 -0300

    Adicionando projeto

---
+++ clientes/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from clientes.models import Cliente
+from clientes.validators import *
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({"cpf":"O CPF é inválido"})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({"nome":"O nome deve conter apenas letras"})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({"rg": "O RG deve ter 9 dígitos"})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({"celular":"O número de celular deve seguir este modelo: 11 12345-1234"})
+        return data
+
+
+    
+        
+    
